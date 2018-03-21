3c763eb02483eebdf7f2b32a6bb6543834265664
canssidle wairimu <canssidlewairimu@gmail.com>
2018-03-21 17:00:45 +0300

    add block elements

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var add = function(Number1, Number2) {
+return Number1+ Number2
+};
+alert(add(3,5));
+var Number1 = parseInt(prompt("Enter a number:")); 
+var Number2 = parseInt(prompt("Enter another number:"));
+alert(add(Number1,Number2));
